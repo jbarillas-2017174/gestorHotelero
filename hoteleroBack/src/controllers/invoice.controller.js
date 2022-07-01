0ec0e5aae8d502493a26b675e50f6d400f160c59
Barillas <jbarillas-2017174@kinal.edu.gt>
2022-07-01 15:37:43 -0600

    Bug  invoice resolved

--- hoteleroBack/src/controllers/invoice.controller.js
+++ hoteleroBack/src/controllers/invoice.controller.js
@@ -7,15 +7,15 @@ exports.createHistory = async (req, res, rooms, users, tot) => {
         const data = {
             user: users,
             room: rooms,
-            total: tot,
+            subtotal: tot,
             times: 1
         }
         const his = await History.findOne({ user: data.user, room: data.room })
-
+        
         if (his) {
             const times = his.times + 1
-            const total = his.total + tot
-            await History.findOneAndUpdate({ _id: his.id }, { total: total, times: times }, { new: true })
+            const total = his.subtotal + tot
+            await History.findOneAndUpdate({ _id: his.id }, { subtotal: total, times: times }, { new: true })
         } else {
             const history = new History(data);
             await history.save();
