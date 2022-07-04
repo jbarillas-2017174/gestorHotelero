88bb917170f3d78b5a3fa1ad3f874677ef73eca2
Barillas <jbarillas-2017174@kinal.edu.gt>
2022-07-04 13:58:59 -0600

    Se agregaron las vistas restantes

--- Hotelero-Front/Front-Hotelero/src/app/app-routing.module.ts
+++ Hotelero-Front/Front-Hotelero/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HistoryComponent } from './components/history/history.component';
+import { InvoiceComponent } from './components/invoice/invoice.component';
 import { LoginComponent } from './components/login/login.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from './components/register/register.component';
+import { UsersComponent } from './components/users/users.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'users', component: UsersComponent},
+  {path: 'history', component: HistoryComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'invoice', component: InvoiceComponent},
+  {path: '**', component: NotfoundComponent}
 ];
 
 @NgModule({
--- Hotelero-Front/Front-Hotelero/src/app/app.module.ts
+++ Hotelero-Front/Front-Hotelero/src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { EventsComponent } from './components/events/events.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HistoryComponent } from './components/history/history.component';
+import { UsersComponent } from './components/users/users.component';
+import { InvoiceComponent } from './components/invoice/invoice.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    EventsComponent,
+    ProfileComponent,
+    HistoryComponent,
+    UsersComponent,
+    InvoiceComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
