ed68eed4b605c1c761b5310f952d4794d0524ca3
ecastellanos-2017333 <ecastellanos-2017333@kinal.edu.gt>
2022-07-15 17:03:14 -0600

    Se encontró bug #1

--- Hotelero-Front/Front-Hotelero/src/app/components/rooms/rooms.component.ts
+++ Hotelero-Front/Front-Hotelero/src/app/components/rooms/rooms.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RoomsRestService } from 'src/app/services/roomsRest/rooms-rest.service';
+import { ModelRooms } from 'src/app/models/rooms.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-rooms',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rooms.component.css']
 })
 export class RoomsComponent implements OnInit {
+  idHotel: any
+  rooms: ModelRooms
+  room: any
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(public activatedRoute: ActivatedRoute,
+    private roomRest: RoomsRestService) {
+    this.rooms = new ModelRooms('', 0, '', true, 0, '', '')
   }
 
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((id: any) => {
+      this.idHotel = id.get('id');
+      console.log(this.idHotel)
+
+    })
+    this.getRooms();
+  }
+
+  getRooms() {
+    this.roomRest.getRooms().subscribe({
+      next: (res:any) =>console.log(res),
+      error: (err)=>alert(err.error.message || err.error)
+    })
+  }
+
+
 }
+
